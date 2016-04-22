cca22a966644a020935f57da7f66cc0bba7bea98
Toesty <hoest97@gmail.com>
2016-04-22 14:55:55 -0500

    Update 2DArrays

--- Array2DExercises/src/Array2DExercises.java
+++ Array2DExercises/src/Array2DExercises.java
@@ -1,72 +1,4 @@
 public class Array2DExercises {
-<<<<<<< HEAD
-	
-		// return the total of all the values in the array.
-		public static int getTotal(int[][] arr) {
-			 int sum = 0;
-		        for(int i = 0; i < arr.length; i++) {
-		                for(int j = 0; j < arr[i].length; j++) {
-		                        sum += arr[i][j];
-		                }
-		        }
-		        return sum;
-		}
-		
-		// return the average of all the values in the array.
-		public static double getAverage(int[][] arr) {
-			 int sum = 0;
-		        for(int i = 0; i < arr.length; i++) {
-		                for(int j = 0; j < arr[i].length; j++) {
-		                        sum = (arr[i][j])/(arr[i].length);
-		                }
-		        }
-		        return sum;
-		}
-		
-		// return the total of the values in the specified row.
-		public static int getRowTotal(int[][] a, int row) {
-			return -1;
-		}
-		
-		// return the total of the values in the specified column.
-		public static int getColumnTotal(int[][] a, int column) {
-			return -1;
-		}
-		
-		// return the highest value in the specified row of the array.
-		public static int getHighestInRow(int[][] a, int row) {
-			return -1;
-		}
-		
-		// return the lowest value in the specified row of the array.
-		public static int getLowestInRow(int[][] a, int row) {
-			return -1;
-		}
-		
-		// return the lowest value in the specified column of the array
-		public static int getHighestInColumn(int[][] a, int column) {
-			return -1;
-		}
-		
-		// return the highest value in the specified column of the array
-		public static int getLowestInColumn(int[][] a, int column) {
-			return -1;
-		}
-		
-		// return the diagonal sum, bottom left to top right
-		public static int getBottomLeftToTopRightDiagonalSum(int[][] a) {
-			return -1;
-		}
-		
-		// return the diagonal sum, top left to bottom right
-		public static int getTopLeftToBottomRightDiagonalSum(int[][] a) {
-			return -1;
-		}
-		
-		// return true if the array is sorted low to high
-		public static boolean isArraySorted(int[][] a) {
-			return false;
-=======
 
 	// return the total of all the values in the array.
 	public static int getTotal(int[][] a) {
@@ -75,7 +7,6 @@ public class Array2DExercises {
 			for (int j = 0; j < a[i].length; j++) {
 				sum += a[i][j];
 			}
->>>>>>> fa51bc0051f616bed11f02d1fa819935f178bb9e
 		}
 		return sum;
 	}
@@ -123,8 +54,8 @@ public class Array2DExercises {
 
 	// return the diagonal sum, bottom left to top right
 	public static int getBottomLeftToTopRightDiagonalSum(int[][] a) {
-		for(int i = 0; i < a.length; i++) {
-			
+		for (int i = 0; i < a.length; i++) {
+
 		}
 		return -1;
 	}
@@ -145,42 +76,66 @@ public class Array2DExercises {
 	}
 
 	// return true if array is jagged
-	
-	public static boolean isJaggedArray(int[][] a) {
-		for(int i = 0; i < a.length; i++) {
-	        System.out.print("\n" + i + ": ");
-	        for(int j = 0; j < a[i].length; j++) {
-	                System.out.print(j + ",");
-	        }
-	}
-		return false;
+
+	public static boolean isJaggedArray(int[][] arr) {
+		
+		boolean isJagged = false;
+
+		if (arr != null) {
+			Integer lastLength = null;
+			for (int i = 0; i < arr.length; i++) {
+				if (lastLength == null) {
+					lastLength = arr[i].length;
+				} else if (lastLength.equals(arr[i].length)) {
+					continue;
+				} else {
+					isJagged = true;
+					break;
+				}
+			}
+		}
+		return isJagged;
 	}
 
 	// return true if array is square (n x n)
-	//just find if the boxes make a square or not
-	public static boolean isSquareArray(int[][] a) {
-		 
-		int product = 0;
-		for (int m = 0; m < a.length; m++){
-			for (int n = 0; n < a[n].length; n++) {
-				product *= a[n][m];
-			}
-		}
-		return false;
-	}
+	// just find if the boxes make a square or not
+	public static boolean isSquareArray(int[][] arr) {
+		 boolean isSquare = false;
+
+	        if (arr != null) {
+	            for (int i = 0; i < arr.length; i++) {
+	                if (arr[i].length != arr.length) {
+	                    break;
+	                } else if (i != arr.length - 1) {
+	                    continue;
+	                } else {
+	                    isSquare = true;
+	                }
+	            }
+	        }
+
+	        return isSquare;
+	    }
+
 
 	// return true if array is rectangular (n x m)
-	//find if it makes a square or not
+	// find if it makes a square or not
 	public static boolean isRectangularArray(int[][] arr) {
+		 boolean isSquare = true;
 
-		int product = 0;
-		for (int m = 0; m < arr.length; m++) {
-			for (int n = 0; n < arr[n].length; n++) {
-				product *= arr[n][m];
-			}
+	        if (arr != null) {
+	            for (int i = 0; i < arr.length; i++) {
+	                if (arr[i].length != arr.length) {
+	                    break;
+	                } else if (i != arr.length - 1) {
+	                    continue;
+	                } else {
+	                    isSquare = false;
+	                }
+	            }
+	        }
 
-			return false;
-		}
-		return false;
-	}
+	        return isSquare;
+	    }
 }
+
